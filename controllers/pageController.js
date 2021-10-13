c3dcac6425ec660b38501beaa175b17043f46a03
tanselberkant <tanselberkant@gmail.com>
2021-10-14 01:02:55 +0300

    :lipstick: Nodemailer

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,5 +1,6 @@
 const logger = require('../logger')
 const User = require('../models/User');
+const nodemailer = require('nodemailer');
 
 exports.getIndexPage = (req, res) => {
   logger.info(req.session.userID);
@@ -47,4 +48,50 @@ exports.getLoginPage = (req,res) => {
 
 exports.getRegisterPage = (req,res) => {
   res.status(200).render('register');
+}
+
+exports.sendEMail = async (req,res) => {
+  try {
+    const outputMessage = `
+    <h1> Mail Details </h1>
+    <ul>
+      <li> Name : ${req.body.name} </li>
+      <li> Email : ${req.body.email} </li>
+      <li> Phone : ${req.body.phone} </li>
+    </ul>
+    <h1> Message </h1>
+    <p>${req.body.message}</p>
+    `;
+
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'berkantostanselix@gmail.com', // generated ethereal user
+        pass: 'ofscosrmiplszleu', // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Fithub🏋️‍♂️" <berkantostanselix@gmail.com>', // sender address
+      to: 'tanselberkant@gmail.com', // list of receivers
+      subject: 'Fithub🏋️‍♂️ | New Message', // Subject line
+      html: outputMessage, // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    req.flash("success","We received your message successfully")
+    return res.status(200).redirect('/contact');
+  } catch (error) {
+    req.flash("error", 'Something bad happened !')
+    return res.status(200).redirect('/contact')
+  }
 }
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -8,6 +8,7 @@ router.route('/').get(pageController.getIndexPage);  // localhost:3000
 router.route('/about').get(pageController.getAboutPage);
 router.route('/trainers').get(pageController.getTrainersPage);
 router.route('/contact').get(pageController.getContactPage);
+router.route('/contact').post(pageController.sendEMail);
 router.route('/gallery').get(pageController.getGalleryPage);
 router.route('/login').get(redirectMiddleware, pageController.getLoginPage);
 router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);
